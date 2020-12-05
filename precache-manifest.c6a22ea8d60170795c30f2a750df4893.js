585c5a1050a1e449b225684c39824ae025502d43
Swapnil Rajawat <swapnilrajawat15121998@gmail.com>
2020-12-05 21:49:20 +0530

    Updates

---
+++ precache-manifest.c6a22ea8d60170795c30f2a750df4893.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b89fecd8ce843c491d987d21ce385a10",
+    "url": "/Speed-Typing-using-React-Hooks/index.html"
+  },
+  {
+    "revision": "8b82f91108989845a1ac",
+    "url": "/Speed-Typing-using-React-Hooks/static/css/main.f2eb72df.chunk.css"
+  },
+  {
+    "revision": "403bd7eb65b3d2cd7686",
+    "url": "/Speed-Typing-using-React-Hooks/static/js/2.ebd63b2b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Speed-Typing-using-React-Hooks/static/js/2.ebd63b2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b82f91108989845a1ac",
+    "url": "/Speed-Typing-using-React-Hooks/static/js/main.dbe3adba.chunk.js"
+  },
+  {
+    "revision": "7386f78b613b09015b26",
+    "url": "/Speed-Typing-using-React-Hooks/static/js/runtime-main.2a242ebc.js"
+  }
+]);
